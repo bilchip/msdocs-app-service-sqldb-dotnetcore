7557fea48bdc7e49d1b67308a97b2af349f0471a
Ihor Bieshulia <bilchip@gmail.com>
2024-03-13 10:10:51 +0200

    Add AppInsight change connection string

--- DotNetCoreSqlDb/Program.cs
+++ DotNetCoreSqlDb/Program.cs
@@ -6,9 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add database context and cache
 // bilchip@gmail.com: link: https://learn.microsoft.com/en-us/azure/app-service/tutorial-dotnetcore-sqldb-app
 // commit 01
+// commit 02
+// prepare for database
+// add user and assign role
+// SQL> CREATE USER db01_user WITH PASSWORD = 'PP01-Gjkmpjdfntkm-85'
+// SQL> EXEC sp_addrolemember 'db_owner', 'db01_user';
+
 
 builder.Services.AddDbContext<MyDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+  //options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
 builder.Services.AddDistributedMemoryCache();
 
 // Add services to the container.
@@ -16,6 +23,7 @@ builder.Services.AddControllersWithViews();
 
 // Add App Service logging
 builder.Logging.AddAzureWebAppDiagnostics();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 
